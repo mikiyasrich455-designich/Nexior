f10ff29ed8580345c38f4e5e0ae0dba0e0e6571b
mikeweb3 <mikiyasrich455@gmail.com>
2026-04-19 07:15:09 -0200

    Update pixverse.ts

--- src/constants/pixverse.ts
+++ src/constants/pixverse.ts
@@ -1,6 +1,6 @@
 export const PIXVERSE_SERVICE_ID = '74e74695-ceff-49d1-ac84-cfa876225ae8';
 
-export const PIXVERSE_LOGO = 'https://cdn.acedata.cloud/viy61r.jpg';
+export const PIXVERSE_LOGO = 'https://i.postimg.cc/WzQX6Zs2/Gemini-Generated-Image-vbu1gdvbu1gdvbu1.png';
 
 export const PIXVERSE_DEFAULT_MODEL = 'v3.5';
 export const PIXVERSE_DEFAULT_STYLE = '';
