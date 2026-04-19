3ec975376bfc8a16dc1e5f609be3a19650a9896f
mikeweb3 <mikiyasrich455@gmail.com>
2026-04-19 06:47:31 -0200

    Update hailuo.ts

--- src/constants/hailuo.ts
+++ src/constants/hailuo.ts
@@ -1,6 +1,6 @@
 export const HAILUO_SERVICE_ID = '5eaa73b7-b9a7-4a17-afb3-f658fd5495b0';
 
-export const HAILUO_LOGO = 'https://cdn.acedata.cloud/0qg4gp.png';
+export const HAILUO_LOGO = 'https://i.postimg.cc/Qt8C6Yfd/Gemini-Generated-Image-54juu354juu354ju.png';
 
 export const HAILUO_DEFAULT_MODEL = 'minimax-t2v';
 export const HAILUO_DEFAULT_INGREDIENTS_MODEL = 'creative';
