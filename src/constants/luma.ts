485cf456826da817c4dcdba52759d64dd35766fc
mikeweb3 <mikiyasrich455@gmail.com>
2026-04-19 06:43:51 -0200

    Update luma.ts

--- src/constants/luma.ts
+++ src/constants/luma.ts
@@ -1,6 +1,6 @@
 export const LUMA_SERVICE_ID = 'dff45a0d-2858-4936-8e2b-3d49c52aff11';
 
-export const LUMA_LOGO = 'https://cdn.acedata.cloud/ahjfwi.png';
+export const LUMA_LOGO = 'https://i.postimg.cc/mDyMkF5T/Gemini-Generated-Image-xuvy9bxuvy9bxuvy.png';
 
 export const LUMA_DEFAULT_ENHANCEMENT = true;
 export const LUMA_DEFAULT_LOOP = false;
