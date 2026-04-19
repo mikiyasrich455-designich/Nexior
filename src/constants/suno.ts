9250aaba2dc54468a5355626a713e788cc896ed6
mikeweb3 <mikiyasrich455@gmail.com>
2026-04-19 04:57:27 -0200

    Update suno.ts

--- src/constants/suno.ts
+++ src/constants/suno.ts
@@ -1,6 +1,6 @@
 export const SUNO_SERVICE_ID = 'f2b646d8-3cfd-46ef-969a-1ea9eebde329';
 
-export const SUNO_LOGO = 'https://cdn.acedata.cloud/l3ffw7.jpg';
+export const SUNO_LOGO = 'https://i.ibb.co/S4gGt1k7/1000070147.png';
 
 export const SUNO_DEFAULT_QRW = 2;
 export const SUNO_DEFAULT_STEPS = 20;
